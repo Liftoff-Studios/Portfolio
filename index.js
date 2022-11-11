c676e35205df63ab3e18c97c6afdf574b0fde0b5
Abhijit Biswas <84321034+Liftoff-Studios@users.noreply.github.com>
2022-11-11 20:06:23 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-let express = require("express");
-let app = express();
-const path = require('path');
-
-app.use("/",express.static(path.join(__dirname,"/Resources")))
-  
-app.listen(3000);
-module.exports = app;
